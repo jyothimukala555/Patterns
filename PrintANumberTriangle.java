8918fb3e950c7d598e303ec423674880de47003e
jyothimukala555 <138197142+jyothimukala555@users.noreply.github.com>
2023-08-12 23:23:11 +0530

    Create PrintANumberTriangle.java

---
+++ PrintANumberTriangle.java
@@ -0,0 +1,31 @@
+package com.java;
+
+public class PrintANumberTriangle {
+     
+	private int number;
+	
+	public PrintANumberTriangle(int number) {
+		this.number = number;
+	}
+	
+	
+	public static void main(String [] args) {
+		PrintANumberTriangle printTriangle = new PrintANumberTriangle(5);
+		printTriangle.printNumbertTriangle();
+		
+	}
+	
+	public void printNumbertTriangle() {
+//		1 
+//		1 2 
+//		1 2 3 
+//		1 2 3 4 
+//		1 2 3 4 5 
+		for(int i=1;i<=number;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j +" ");
+			}
+			System.out.println();
+		}
+	}
+}
